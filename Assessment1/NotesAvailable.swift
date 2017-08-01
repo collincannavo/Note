b5629b52405a76ef67d13d87ad925fc98eb25fda
Bop <collin.cannavo@gmail.com>
2017-07-31 19:53:18 -0600

    Removed unused function

--- Assessment1/NotesAvailable.swift
+++ Assessment1/NotesAvailable.swift
@@ -13,13 +13,5 @@ class NotesAvailable {
     
     let name = "FirstName"
     let lastName = "LastName"
-    
-    let array: [Array] = [] as! [Array]
-    
-    func setupStuff() {
-        for name in array {
-            one too many
-        }
         
-    }
 }
